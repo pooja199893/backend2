761925736a5981ce0471b5ce9e6e850aa4021aa7
pooja199893 <poojacrane15111998@gmail.com>
2025-04-30 10:16:46 +0530

    updates

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ import { Router } from "express";
 import AuthRoutes from "./auth.routes.js";
 import ProductRoutes from "./product.routes.js";
 import UserRoutes from "./user.routes.js";
+import OperatorsRoutes from './operator.routes.js'
+
 
 
 const AllRoutes = Router();
@@ -10,6 +12,8 @@ const AllRoutes = Router();
 AllRoutes.use("/auth", AuthRoutes);
 AllRoutes.use("/product", ProductRoutes);
 AllRoutes.use("/user", UserRoutes);
+AllRoutes.use("/operators", OperatorsRoutes);
+
 
 
 
